c9c9ec9fa00b0600d91022198ab04333d89a4c21
James McNally <james@wiresmithtech.com>
2023-02-11 16:56:46 +0000

    Add Chrono Conversions

--- labview-interop/src/timestamp.rs
+++ labview-interop/src/timestamp.rs
@@ -1,3 +1,16 @@
+//! Support for exchanging passwords with LabVIEWs timestamp format.
+//!
+//! This includes binary formats, to and from 1904 epoch, unix (1970) epoch
+//! and optionally chrono DateTime with the `chrono` feature.
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum LVTimeError {
+    #[error("Cannot generate a chrono time as it is out of range.")]
+    ChronoOutOfRange,
+}
+
 /// Mirrors the internal LabVIEW timestamp structure so
 /// it can be passed back and forward.
 #[repr(transparent)]
@@ -11,6 +24,17 @@ pub const UNIX_EPOCH_IN_LV_SECONDS: f64 = 2082844800.0;
 // * from/to bytes
 
 impl LVTime {
+    /// Extract the sub-second component as a floating point number.
+    pub fn sub_seconds(&self) -> f64 {
+        let fractional = self.0 & 0xFFFF_FFFF_FFFF_FFFF;
+        (fractional as f64) / 0xFFFF_FFFF_FFFF_FFFFu64 as f64
+    }
+
+    ///Extract the seconds component which is referenced to the LabVIEW epoc.
+    pub fn seconds(&self) -> u64 {
+        (self.0 >> 64) as u64
+    }
+
     /// From a double precision number which is the seconds
     /// since the 1904 epoch used by LabVIEW
     pub fn from_lv_epoch(seconds: f64) -> Self {
@@ -22,12 +46,19 @@ impl LVTime {
     /// Into a double precision number which is the seconds
     /// since the 1904 epoch used by LabVIEW.
     pub fn to_lv_epoch(&self) -> f64 {
-        let (seconds, fractions) = self.to_parts();
-        let seconds: f64 = seconds as f64;
-        //let fractions: f64 = fractions as f64;
-        println!("{fractions}");
-        let fractions: f64 = (fractions as f64) / 0xFFFF_FFFF_FFFF_FFFFu64 as f64;
-        seconds + fractions
+        self.seconds() as f64 + self.sub_seconds()
+    }
+
+    /// To a double precision number which is the seconds since unix epoch.
+    pub fn to_unix_epoch(&self) -> f64 {
+        let lv_epoch = self.to_lv_epoch();
+        lv_epoch - UNIX_EPOCH_IN_LV_SECONDS
+    }
+
+    /// To a double precision number which is the seconds since unix epoch.
+    pub fn from_unix_epoch(seconds: f64) -> Self {
+        let lv_epoch = seconds + UNIX_EPOCH_IN_LV_SECONDS;
+        Self::from_lv_epoch(lv_epoch)
     }
 
     /// Build from the full seconds and fractional second parts.
@@ -37,10 +68,9 @@ impl LVTime {
     }
 
     /// Seperate out the u64 components.
-    fn to_parts(&self) -> (u64, u64) {
-        let seconds = (self.0 >> 64) as u64;
+    pub fn to_parts(&self) -> (u64, u64) {
         let fractions = (self.0 & 0xFFFF_FFFF_FFFF_FFFF) as u64;
-        (seconds, fractions)
+        (self.seconds(), fractions)
     }
 
     /// To little endian bytes.
@@ -64,6 +94,33 @@ impl LVTime {
     }
 }
 
+#[cfg(feature = "chrono")]
+mod chrono {
+
+    use super::*;
+    use ::chrono::{DateTime, NaiveDateTime, Utc};
+
+    impl TryFrom<LVTime> for DateTime<Utc> {
+        type Error = LVTimeError;
+
+        fn try_from(value: LVTime) -> Result<Self, Self::Error> {
+            let naive_time: NaiveDateTime = value.try_into()?;
+            Ok(DateTime::<Utc>::from_utc(naive_time, Utc))
+        }
+    }
+
+    impl TryFrom<LVTime> for NaiveDateTime {
+        type Error = LVTimeError;
+
+        fn try_from(value: LVTime) -> Result<Self, Self::Error> {
+            let seconds_for_time: i64 = value.seconds() as i64 - UNIX_EPOCH_IN_LV_SECONDS as i64;
+            let nanoseconds = value.sub_seconds() * 1_000_000_000f64;
+            Self::from_timestamp_opt(seconds_for_time, nanoseconds as u32)
+                .ok_or(LVTimeError::ChronoOutOfRange)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,6 +136,13 @@ mod tests {
         let time = LVTime::from_parts(20, 0x8000_0000_0000_0000);
         assert_eq!(20.5f64, time.to_lv_epoch());
         assert_eq!(time, LVTime::from_lv_epoch(20.5f64));
+    }
+
+    #[test]
+    fn test_to_from_unix_epoch() {
+        let time = LVTime::from_parts(3758974472, 0x8000_0000_0000_0000);
+        assert_eq!(1676129672.5f64, time.to_unix_epoch());
+        assert_eq!(time, LVTime::from_unix_epoch(1676129672.5f64));
     }
 
     #[test]
@@ -103,5 +167,33 @@ mod tests {
             [00, 00, 00, 00, 00, 00, 00, 0x14, 0x80, 00, 00, 00, 00, 00, 00, 00]
         );
         assert_eq!(time, LVTime::from_be_bytes(bytes));
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "chrono")]
+mod chrono_tests {
+    use crate::timestamp::UNIX_EPOCH_IN_LV_SECONDS;
+
+    use super::LVTime;
+    use chrono::NaiveDateTime;
+    use chrono::{DateTime, Utc};
+
+    #[test]
+    fn datetime_from_lv_time() {
+        let date_time: DateTime<Utc> = LVTime::from_lv_epoch(3758974472.02440977f64)
+            .try_into()
+            .unwrap();
+        let naive: NaiveDateTime = LVTime::from_lv_epoch(3758974472.02440977f64)
+            .try_into()
+            .unwrap();
+        let expected_naive = NaiveDateTime::from_timestamp_opt(
+            3758974472 - UNIX_EPOCH_IN_LV_SECONDS as i64,
+            024409770,
+        )
+        .unwrap();
+        let expected = DateTime::<Utc>::from_utc(expected_naive, Utc);
+        assert_eq!(date_time, expected);
+        assert_eq!(naive, expected_naive)
     }
 }
