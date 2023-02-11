834f870a63f10acb3a6bd1d06fa22b43b4739fd7
James McNally <james@wiresmithtech.com>
2023-02-11 15:56:39 +0000

    Basic Timestamp Support
    Added the timestamp support minus any DateTime conversion in Rust.

--- labview-interop/src/timestamp.rs
+++ labview-interop/src/timestamp.rs
@@ -5,7 +5,7 @@
 pub struct LVTime(u128);
 
 ///The Unix Epoch in LabVIEW epoch seconds for shifting timestamps between them.
-pub const UNIX_EPOCH_IN_LV_SECONDS: f64 = 2082844800;
+pub const UNIX_EPOCH_IN_LV_SECONDS: f64 = 2082844800.0;
 
 //todo:
 // * from/to bytes
@@ -13,7 +13,7 @@ pub const UNIX_EPOCH_IN_LV_SECONDS: f64 = 2082844800;
 impl LVTime {
     /// From a double precision number which is the seconds
     /// since the 1904 epoch used by LabVIEW
-    fn from_lv_epoch(seconds: f64) -> Self {
+    pub fn from_lv_epoch(seconds: f64) -> Self {
         let (seconds, fractions) = (seconds / 1.0, seconds % 1.0);
         let integer_fractions = (fractions * 0xFFFF_FFFF_FFFF_FFFFu64 as f64) as u64;
         Self::from_parts(seconds as u64, integer_fractions)
@@ -21,7 +21,7 @@ impl LVTime {
 
     /// Into a double precision number which is the seconds
     /// since the 1904 epoch used by LabVIEW.
-    fn to_lv_epoch(&self) -> f64 {
+    pub fn to_lv_epoch(&self) -> f64 {
         let (seconds, fractions) = self.to_parts();
         let seconds: f64 = seconds as f64;
         //let fractions: f64 = fractions as f64;
@@ -31,15 +31,36 @@ impl LVTime {
     }
 
     /// Build from the full seconds and fractional second parts.
-    fn from_parts(seconds: u64, fractions: u64) -> Self {
+    pub fn from_parts(seconds: u64, fractions: u64) -> Self {
         let time = (seconds as u128) << 64 | (fractions as u128);
         Self(time)
     }
 
+    /// Seperate out the u64 components.
     fn to_parts(&self) -> (u64, u64) {
         let seconds = (self.0 >> 64) as u64;
         let fractions = (self.0 & 0xFFFF_FFFF_FFFF_FFFF) as u64;
         (seconds, fractions)
+    }
+
+    /// To little endian bytes.
+    pub fn to_le_bytes(&self) -> [u8; 16] {
+        self.0.to_le_bytes()
+    }
+
+    /// To big endian bytes.
+    pub fn to_be_bytes(&self) -> [u8; 16] {
+        self.0.to_be_bytes()
+    }
+
+    /// From little endian bytes.
+    pub fn from_le_bytes(bytes: [u8; 16]) -> Self {
+        Self(u128::from_le_bytes(bytes))
+    }
+
+    /// From big endian bytes.
+    pub fn from_be_bytes(bytes: [u8; 16]) -> Self {
+        Self(u128::from_be_bytes(bytes))
     }
 }
 
@@ -58,5 +79,29 @@ mod tests {
         let time = LVTime::from_parts(20, 0x8000_0000_0000_0000);
         assert_eq!(20.5f64, time.to_lv_epoch());
         assert_eq!(time, LVTime::from_lv_epoch(20.5f64));
+    }
+
+    #[test]
+    fn test_to_from_le_bytes() {
+        let time = LVTime::from_parts(20, 0x8000_0000_0000_0000);
+        assert_eq!(time.0, 0x14_8000_0000_0000_0000);
+        let bytes = time.to_le_bytes();
+        assert_eq!(
+            bytes,
+            [00, 00, 00, 00, 00, 00, 00, 0x80, 0x14, 00, 00, 00, 00, 00, 00, 00]
+        );
+        assert_eq!(time, LVTime::from_le_bytes(bytes));
+    }
+
+    #[test]
+    fn test_to_from_be_bytes() {
+        let time = LVTime::from_parts(20, 0x8000_0000_0000_0000);
+        assert_eq!(time.0, 0x14_8000_0000_0000_0000);
+        let bytes = time.to_be_bytes();
+        assert_eq!(
+            bytes,
+            [00, 00, 00, 00, 00, 00, 00, 0x14, 0x80, 00, 00, 00, 00, 00, 00, 00]
+        );
+        assert_eq!(time, LVTime::from_be_bytes(bytes));
     }
 }
