9fb7bc2066570ed75f4d3184f15fd2fc057dc765
Johannes Kneer <johannes.kneer@nuflo.eu>
2024-07-30 23:30:12 +0200

    Cleaned error list

--- labview-interop/src/errors.rs
+++ labview-interop/src/errors.rs
@@ -3,10 +3,48 @@ use thiserror::Error;
 
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
-// Macro to define the enum and implement the conversions
+/// # Error handling
+///
+/// There are four error paths that the labview-interop crate needs to handle:
+///
+/// 1. **MgError and MgErrorCode**, Internal: LabView Memory Manager --> Rust
+///    This crate calls the labview memory manager internally to deal with memory
+///    owned by LabVIEW. The functions of the memory manager return MgErr. The documentation
+///    on https://www.ni.com/docs/en-US/bundle/labview/page/labview-manager-function-errors.html
+///    gives a full list of possible error values.
+///
+/// 2. **MgErrorCode**: to LabVIEW through function return
+///    We want to be able to return the errors genated internally through the function return and be
+///    understood on the LabVIEW side. This is straight forward for Errors of MgError type. But we will
+///    have an internal compound Error type that can have a different type. When using status returns, these
+///    can only be converted to a very generic error code. Therefore 3
+///
+/// 3. **InteropError**: to LabVIEW through ErrorCluster parameter
+///    Our internal `LvInteropError` compound error can easily be converted to an ErrorCluster. For MgErrors the conversion is
+///    straight forward. The correct source descriptions are gotten from the memory manager through `NIGetOneErrorCode`.
+///    For non LV errors, a generic error is leveraged, and the source description is overwritten.
+///
+/// 4. from LabVIEW through ErrorCluster parameter
+///    Will labview-interop ever need to make sense of an error? It may be good enough to differentiate between an error and
+///    warnings. TBD
+///
+/// # Error Implementation
+/// The `MgError` / `MgErrorCode` implement From in both directions. Additionally IntoPrimitive and TryFromPrimitive is derived
+/// to enable the conversion from and to int primitives.
+///
+/// LabVIEW official general error list:
+/// https://www.ni.com/docs/en-US/bundle/labview-api-ref/page/errors/general-labview-error-codes.html
+/// more complete inofficial lists:
+/// https://labviewwiki.org/wiki/LabVIEW_Error_Code_Family   /  https://labviewwiki.org/wiki/List_of_errors
+///
+/// Accoring to https://www.ni.com/docs/en-US/bundle/labview/page/labview-manager-function-errors.html
+/// the memory manager only uses a subset of this huge error list. The subset is implemented in `MgError` using
+/// the official abbreviations.
+
+// Macro to define the MgError and MgErrorCode and the From conversions
 macro_rules! define_errors {
     ($(($name:ident, $code:expr, $msg:expr)),*) => {
-        #[derive(Debug, IntoPrimitive, TryFromPrimitive)]
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, IntoPrimitive, TryFromPrimitive)]
         #[repr(i32)]
         pub enum MgErrorCode {
             $(
@@ -14,7 +52,7 @@ macro_rules! define_errors {
             )*
         }
 
-        #[derive(Debug, Error)]
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Error)]
         pub enum MgError {
             $(
                 #[error($msg)]
@@ -44,143 +82,114 @@ macro_rules! define_errors {
     };
 }
 
-// LabVIEW general errors:
-// https://www.ni.com/docs/en-US/bundle/labview-api-ref/page/errors/general-labview-error-codes.html
-// Maybe these description match up wiht some of the Memory Manager errors?
-// TOdo: do we need to implement those error codes?
-
-// Define the Memory Manager errors using the macro
 define_errors!(
-    (MgArgErr, 1, "Argument Error"),
-    (MFullErr, 2, "Memory Full Error"),
-    (FEof, 4, "End of File"),
-    (FIsOpen, 5, "File is Open"),
-    (FIoErr, 6, "I/O Error"),
-    (FNotFound, 7, "File Not Found"),
-    (FNoPerm, 8, "No Permission"),
-    (FDiskFull, 9, "Disk Full"),
-    (FDupPath, 10, "Duplicate Path"),
-    (FtMFOpen, 11, "File Table Memory Full"),
-    (FNotEnabled, 12, "File Not Enabled"),
-    (RfNotFound, 13, "Reference Not Found"),
-    (RAddFailed, 14, "Addition Failed"),
-    (RNotFound, 15, "Resource Not Found"),
-    (INotFound, 16, "Item Not Found"),
-    (IMemoryErr, 17, "Memory Error"),
-    (DPenNotExist, 18, "Dependency Not Exist"),
-    (CfgBadType, 19, "Configuration Bad Type"),
-    (CfgTokenNotFound, 20, "Configuration Token Not Found"),
-    (CfgParseError, 21, "Configuration Parse Error"),
-    (CfgAllocError, 22, "Configuration Allocation Error"),
-    (EcLVSBFormatError, 23, "LVSB Format Error"),
-    (EcLVSBSubrError, 24, "LVSB Subroutine Error"),
-    (EcLVSBNoCodeError, 25, "LVSB No Code Error"),
-    (WNullWindow, 26, "Null Window"),
-    (WDestroyMixup, 27, "Destroy Mixup"),
-    (MenuNullMenu, 28, "Null Menu"),
-    (PAbortJob, 29, "Abort Job"),
-    (PBadPrintRecord, 30, "Bad Print Record"),
-    (PDriverError, 31, "Driver Error"),
-    (PWindowsError, 32, "Windows Error"),
-    (PMemoryError, 33, "Memory Error"),
-    (PDialogError, 34, "Dialog Error"),
-    (PMiscError, 35, "Miscellaneous Error"),
-    (DvInvalidRefnum, 36, "Invalid Refnum"),
-    (DvDeviceNotFound, 37, "Device Not Found"),
-    (DvParamErr, 38, "Parameter Error"),
-    (DvUnitErr, 39, "Unit Error"),
-    (DvOpenErr, 40, "Open Error"),
-    (DvAbortErr, 41, "Abort Error"),
-    (BogusError, 42, "Bogus Error"),
-    (CancelError, 43, "Cancel Error"),
-    (OMObjLowErr, 44, "Object Manager Low Error"),
-    (OMObjHiErr, 45, "Object Manager High Error"),
-    (OMObjNotInHeapErr, 46, "Object Manager Not In Heap Error"),
-    (
-        OMOHeapNotKnownErr,
-        47,
-        "Object Manager Heap Not Known Error"
-    ),
-    (OMBadDPIdErr, 48, "Object Manager Bad DPId Error"),
-    (OMNoDPinTabErr, 49, "Object Manager No DP In Table Error"),
-    (
-        OMMsgOutOfRangeErr,
-        50,
-        "Object Manager Message Out Of Range Error"
-    ),
-    (OMMethodNullErr, 51, "Object Manager Method Null Error"),
-    (OMUnknownMsgErr, 52, "Object Manager Unknown Message Error"),
-    (MgNotSupported, 53, "Not Supported"),
-    (NcBadAddressErr, 54, "Network Controller Bad Address Error"),
-    (NcInProgress, 55, "In Progress"),
-    (NcTimeOutErr, 56, "Timeout Error"),
-    (NcBusyErr, 57, "Busy Error"),
-    (NcNotSupportedErr, 58, "Not Supported Error"),
-    (NcNetErr, 59, "Network Error"),
-    (NcAddrInUseErr, 60, "Address In Use Error"),
-    (NcSysOutOfMem, 61, "System Out Of Memory"),
-    (NcSysConnAbortedErr, 62, "System Connection Aborted Error"),
-    (NcConnRefusedErr, 63, "Connection Refused Error"),
-    (NcNotConnectedErr, 64, "Not Connected Error"),
-    (NcAlreadyConnectedErr, 65, "Already Connected Error"),
-    (NcConnClosedErr, 66, "Connection Closed Error"),
-    (AmInitErr, 67, "Initialization Error"),
-    (OccBadOccurrenceErr, 68, "Bad Occurrence Error"),
-    (OccWaitOnUnBoundHdlrErr, 69, "Wait On Unbound Handler Error"),
-    (OccFunnyQOverFlowErr, 70, "Funny Queue Overflow Error"),
-    (FDataLogTypeConflict, 71, "Data Log Type Conflict"),
-    (
-        EcLVSBCannotBeCalledFromThread,
-        72,
-        "LVSB Cannot Be Called From Thread"
-    ),
-    (AmUnrecognizedType, 73, "Unrecognized Type"),
-    (MCorruptErr, 74, "Corrupt Error"),
-    (EcLVSBErrorMakingTempDLL, 75, "LVSB Error Making Temp DLL"),
-    (EcLVSBOldCIN, 76, "LVSB Old CIN"),
-    (FmtTypeMismatch, 81, "Format Type Mismatch"),
-    (FmtUnknownConversion, 82, "Unknown Conversion"),
-    (FmtTooFew, 83, "Too Few"),
-    (FmtTooMany, 84, "Too Many"),
-    (FmtScanError, 85, "Scan Error"),
-    (LvOLEConvertErr, 87, "OLE Convert Error"),
-    (RtMenuErr, 88, "Runtime Menu Error"),
-    (PwdTampered, 89, "Password Tampered"),
-    (LvVariantAttrNotFound, 90, "Variant Attribute Not Found"),
-    (LvVariantTypeMismatch, 91, "Variant Type Mismatch"),
-    (AxEventDataNotAvailable, 92, "Event Data Not Available"),
-    (AxEventStoreNotPresent, 93, "Event Store Not Present"),
-    (AxOccurrenceNotFound, 94, "Occurrence Not Found"),
-    (AxEventQueueNotCreated, 95, "Event Queue Not Created"),
-    (AxEventInfoNotAvailable, 96, "Event Info Not Available"),
-    (OleNullRefnumPassed, 97, "Null Refnum Passed"),
-    (IviInvalidDowncast, 102, "Invalid Downcast"),
-    (IviInvalidClassSesn, 103, "Invalid Class Session"),
-    (NcSockNotMulticast, 108, "Socket Not Multicast"),
-    (NcSockNotSinglecast, 109, "Socket Not Singlecast"),
-    (NcBadMulticastAddr, 110, "Bad Multicast Address"),
-    (NcMcastSockReadOnly, 111, "Multicast Socket Read Only"),
-    (NcMcastSockWriteOnly, 112, "Multicast Socket Write Only"),
-    (NcDatagramMsgSzErr, 113, "Datagram Message Size Error"),
-    (DataCorruptErr, 116, "Data Corrupt Error"),
-    (RequireFullPathErr, 117, "Require Full Path Error"),
-    (FolderNotExistErr, 118, "Folder Does Not Exist Error"),
-    (NcBtInvalidModeErr, 119, "Bluetooth Invalid Mode Error"),
-    (NcBtSetModeErr, 120, "Bluetooth Set Mode Error"),
-    (
-        MgBtInvalidGUIDStrErr,
-        121,
-        "Bluetooth Invalid GUID String Error"
-    ),
-    (RVersInFuture, 122, "Version In Future")
+    (MgArgErr, 1, "An input parameter is invalid."),
+    (MFullErr, 2, "Memory if full."),
+    (FEof, 4, "End of file encountered."),
+    (FIsOpen, 5, "File already open"),
+    (FIoErr, 6, "Generic file I/O error."),
+    (FNotFound, 7, "File not found"),
+    (FNoPerm, 8, "File permission error."),
+    (FDiskFull, 9, "Disk full"),
+    (FDupPath, 10, "Duplicate path"),
+    (FtMFOpen, 11, "Too many files open."),
+    (FNotEnabled, 12, "Some system capacity necessary for operation is not enabled."),
+    (RfNotFound, 13, "Failed to load dynamic library because of missing external symbols or dependencies, or because of an invalid file format."),
+    (RAddFailed, 14, "Cannot add resource."),
+    (RNotFound, 15, "Resource not found."),
+    (INotFound, 16, "Image not found."),
+    (IMemoryErr, 17, "Not enough memory to manipulate image."),
+    (DPenNotExist, 18, "DPen does not exist."),
+    (CfgBadType, 19, "Configuration type invalid."),
+    (CfgTokenNotFound, 20, "Configuration token not found."),
+    (CfgParseError, 21, "Error occurred parsing configuration string."),
+    (CfgAllocError, 22, "Configuration memory error."),
+    (EcLVSBFormatError, 23, "Bad external code format."),
+    (EcLVSBSubrError, 24, "External subroutine not supported."),
+    (EcLVSBNoCodeError, 25, "External code not present."),
+    (WNullWindow, 26, "Null window."),
+    (WDestroyMixup, 27, "Destroy window error."),
+    (MenuNullMenu, 28, "Null menu."),
+    (PAbortJob, 29, "Print aborted"),
+    (PBadPrintRecord, 30, "Bad print record."),
+    (PDriverError, 31, "Print driver error."),
+    (PWindowsError, 32, "Operating system error during print."),
+    (PMemoryError, 33, "Memory error during print."),
+    (PDialogError, 34, "Print dialog error."),
+    (PMiscError, 35, "Generic print error."),
+    (DvInvalidRefnum, 36, "Invalid device refnum."),
+    (DvDeviceNotFound, 37, "Device not found."),
+    (DvParamErr, 38, "Device parameter error."),
+    (DvUnitErr, 39, "Device unit error."),
+    (DvOpenErr, 40, "Cannot open device."),
+    (DvAbortErr, 41, "Device call aborted."),
+    (BogusError, 42, "Generic error."),
+    (CancelError, 43, "Operation cancelled by user."),
+    (OMObjLowErr, 44, "Object ID too low."),
+    (OMObjHiErr, 45, "Object ID too high."),
+    (OMObjNotInHeapErr, 46, "Object not in heap."),
+    (OMOHeapNotKnownErr, 47,"Unknown heap."),
+    (OMBadDPIdErr, 48, "Unknown object (invalid DefProc)."),
+    (OMNoDPinTabErr, 49, "Unknown object (DefProc not in table)."),
+    (OMMsgOutOfRangeErr, 50, "Message out of range."),
+    (OMMethodNullErr, 51, "Null method."),
+    (OMUnknownMsgErr, 52, "Unknown message."),
+    (MgNotSupported, 53, "Manager call not supported."),
+    (NcBadAddressErr, 54, "The network address is ill-formed."),
+    (NcInProgress, 55, "The network operation is in progress."),
+    (NcTimeOutErr, 56, "The network operation exceeded the user-specified or system time limit."),
+    (NcBusyErr,	57, "The network connection is busy."),
+    (NcNotSupportedErr,	58, "The network function is not supported by the system."),
+    (NcNetErr, 59, "The network is down, unreachable, or has been reset."),
+    (NcAddrInUseErr, 60, "The specified port or network address is currently in use. Select an available port or network address."),
+    (NcSysOutOfMem, 61, "The system could not allocate the necessary memory."),
+    (NcSysConnAbortedErr, 62, "The system caused the network connection to be aborted."),
+    (NcConnRefusedErr, 63, "The network connection was refused by the server."),
+    (NcNotConnectedErr, 64, "The network connection is not yet established."),
+    (NcAlreadyConnectedErr, 65, "The network connection is already established."),
+    (NcConnClosedErr, 66, "The network connection was closed by the peer."),
+    (AmInitErr, 67, "Interapplication Manager initialization error."),
+    (OccBadOccurrenceErr, 68, "Bad occurrence."),
+    (OccWaitOnUnBoundHdlrErr, 69, "Handler does not know what occurrence to wait for."),
+    (OccFunnyQOverFlowErr, 70, "Occurrence queue overflow."),
+    (FDataLogTypeConflict, 71, "File datalog type conflict."),
+    (EcLVSBCannotBeCalledFromThread, 72, "Semaphore not signaled."),
+    (AmUnrecognizedType, 73, "Interapplication Manager unrecognized type error."),
+    (MCorruptErr, 74, "Memory or data structure corrupt."),
+    (EcLVSBErrorMakingTempDLL, 75, "Failed to make temporary DLL."),
+    (EcLVSBOldCIN, 76, "Old CIN version."),
+    (FmtTypeMismatch, 81, "Format specifier type mismatch."),
+    (FmtUnknownConversion, 82, "Unknown format specifier."),
+    (FmtTooFew, 83, "Too few format specifiers."),
+    (FmtTooMany, 84, "Too many format specifiers."),
+    (FmtScanError, 85, "Scan failed. The input string does not contain data in the expected format."),
+    (LvOLEConvertErr, 87, "Error converting to variant."),
+    (RtMenuErr, 88, "Run-time menu error."),
+    (PwdTampered, 89, "Another user tampered with the VI password."),
+    (LvVariantAttrNotFound, 90, "Variant attribute not found."),
+    (LvVariantTypeMismatch, 91, "The data type of the variant is not compatible with the data type wired to the type input."),
+    (AxEventDataNotAvailable, 92, "The ActiveX event data was not available on the queue."),
+    (AxEventStoreNotPresent, 93, "ActiveX event information was not available."),
+    (AxOccurrenceNotFound, 94, "The occurrence associated with the ActiveX event was not found."),
+    (AxEventQueueNotCreated, 95, "The ActiveX event queue could not be created."),
+    (AxEventInfoNotAvailable, 96, "ActiveX event information was not available in the type library."),
+    (OleNullRefnumPassed, 97, "A null or previously deleted refnum was passed in as an input."),
+    (IviInvalidDowncast, 102, "IVI invalid downcast."),
+    (IviInvalidClassSesn, 103, "No IVI class session opened."),
+    (NcSockNotMulticast, 108, "Singlecast connections cannot send to multicast addresses."),
+    (NcSockNotSinglecast, 109, "Multicast connections cannot send to singlecast addresses."),
+    (NcBadMulticastAddr, 110, "Specified IP address is not in multicast address range."),
+    (NcMcastSockReadOnly, 111, "Cannot write to read-only multicast connection."),
+    (NcMcastSockWriteOnly, 112, "Cannot read from write-only multicast connection."),
+    (NcDatagramMsgSzErr, 113, "A message sent on a datagram socket was larger than the internal message buffer or some other network limit, or the buffer used to receive a datagram was smaller than the datagram itself."),
+    (DataCorruptErr, 116, "Unflatten or byte stream read operation failed due to corrupt, unexpected, or truncated data."),
+    (RequireFullPathErr, 117, "Directory path supplied where a file path is required. A file path with the filename is required, but the supplied path is a path to a directory."),
+    (FolderNotExistErr, 118, "The supplied folder path does not exist."),
+    (NcBtInvalidModeErr, 119, "Illegal combination of Bluetooth discoverable and non-connectable modes."),
+    (NcBtSetModeErr, 120, "Error setting Bluetooth mode."),
+    (MgBtInvalidGUIDStrErr, 121, "Invalid GUID string."),
+    (RVersInFuture, 122, "The resource you are attempting to open was created in a more recent version of LabVIEW and is incompatible with this version.")
 );
-
-// Helper to convert ErrorCode to i32
-// impl From<MgErrorCode> for i32 {
-//     fn from(code: MgErrorCode) -> Self {
-//         code as i32
-//     }
-// }
 
 /// MgErr is a simple wrapper around the error code that
 /// is returned by the memory manager functions.
